8a01872cd258af6068bc6942c99cf2b98ca8d2de
Gilad K <gk2610@columbia.edu>
2026-05-28 14:53:26 -0400

    fix ;g telescope

--- nvim/lua/gilad/plugins/telescope.lua
+++ nvim/lua/gilad/plugins/telescope.lua
@@ -60,19 +60,13 @@ return {
         SET_MAP("n",  ";f",    builtin.find_files,           "Search [F]iles")
         SET_MAP("n",  ";r",    builtin.oldfiles,             "Search [R]ecent files")
         SET_MAP("n",  ";w",    builtin.live_grep,            "Search [W]ord")
-        SET_MAP("n", ";g", function()
-            local parent = vim.fn.system("gt log --steps 1 2>/dev/null | grep -oE 'gk/[a-zA-Z0-9_-]+|main' | tail -1"):gsub("\n", "")
-            if parent == "" then
-                  parent = "main"
-            end
-            require("telescope.builtin").git_files({
-            git_command = { "git", "diff", "--name-only", parent },
-            prompt_title = "Git changes vs " .. parent
-          })
-        end, "Search [G]it changes vs parent")
 
-        SET_MAP( "n", "<M-j>", ":cnext<CR>",                 "Quickfix Next match")
-        SET_MAP( "n", "<M-k>", ":cprev<CR>",                 "Quickfix Prev match")
+        SET_MAP("n", ";g", function()
+          require("telescope.builtin").git_files({
+            git_command = { "git", "diff", "--name-only", "main" },
+            prompt_title = "Git changes vs main"
+          })
+        end, "Search [G]it changes vs main")
 		-- stylua: ignore end
 	end,
 }
